ab4f942d25507e0d19de83c2b950039a9cd87a1e
SamPoyigi <sam@sampoyigi.com>
2018-12-16 14:17:34 +0000

    Minor changes to form toolbar button

--- controllers/Banners.php
+++ controllers/Banners.php
@@ -12,15 +12,15 @@ class Banners extends \Admin\Classes\AdminController
     public $listConfig = [
         'list' => [
             'model' => 'Igniter\Frontend\Models\Banners',
-            'title' => 'lang:admin::banners.text_title',
-            'emptyMessage' => 'lang:admin::banners.text_empty',
+            'title' => 'lang:igniter.frontend::default.banners.text_title',
+            'emptyMessage' => 'lang:igniter.frontend::default.banners.text_empty',
             'defaultSort' => ['order_id', 'DESC'],
             'configFile' => 'banners',
         ],
     ];
 
     public $formConfig = [
-        'name' => 'lang:admin::banners.text_form_name',
+        'name' => 'lang:igniter.frontend::default.banners.text_form_name',
         'model' => 'Igniter\Frontend\Models\Banners',
         'create' => [
             'title' => 'lang:admin::lang.form.create_title',
@@ -54,13 +54,13 @@ class Banners extends \Admin\Classes\AdminController
     public function formValidate($model, $form)
     {
         $namedRules = [
-            ['name', 'lang:admin::banners.label_name', 'required|min:2|max:255'],
-            ['type', 'lang:admin::banners.label_type', 'required|alpha|max:8'],
-            ['click_url', 'lang:admin::banners.label_click_url', 'required|min:2|max:255'],
-            ['custom_code', 'lang:admin::banners.label_custom_code', 'required_if:type,custom'],
-            ['alt_text', 'lang:admin::banners.label_alt_text', 'required_if:type,image|min:2|max:255'],
-            ['image_code', 'lang:admin::banners.label_image', 'required_if:type,image'],
-            ['language_id', 'lang:admin::banners.label_language', 'required|integer'],
+            ['name', 'lang:igniter.frontend::default.banners.label_name', 'required|min:2|max:255'],
+            ['type', 'lang:igniter.frontend::default.banners.label_type', 'required|alpha|max:8'],
+            ['click_url', 'lang:igniter.frontend::default.banners.label_click_url', 'required|min:2|max:255'],
+            ['custom_code', 'lang:igniter.frontend::default.banners.label_custom_code', 'required_if:type,custom'],
+            ['alt_text', 'lang:igniter.frontend::default.banners.label_alt_text', 'required_if:type,image|min:2|max:255'],
+            ['image_code', 'lang:igniter.frontend::default.banners.label_image', 'required_if:type,image'],
+            ['language_id', 'lang:igniter.frontend::default.banners.label_language', 'required|integer'],
             ['status', 'lang:admin::lang.label_status', 'required|integer'],
         ];
 
