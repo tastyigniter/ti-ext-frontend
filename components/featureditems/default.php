1a3b36ee18ea4dd7db89be5569ccaa86f32343fe
SamPoyigi <sam@sampoyigi.com>
2019-11-25 01:26:27 +0000

    Hide featured menu items component title when there are no items

--- components/featureditems/default.php
+++ components/featureditems/default.php
@@ -1,33 +1,35 @@
-<div id="featured-menu-box" class="module-box py-5">
-    <div class="container text-center">
-        <div class="row">
-            <div class="col-12">
-                <h2 class="display-5 mb-3"><?= $featuredTitle; ?></h2>
+<?php if (count($featuredMenuItems)) { ?>
+    <div id="featured-menu-box" class="module-box py-5">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="display-5 mb-3"><?= $featuredTitle; ?></h2>
+                </div>
             </div>
-        </div>
 
-        <div class="row">
-            <?php foreach ($featuredMenuItems as $featuredItem) { ?>
-                <div class="col-sm-<?= round(12 / $featuredPerRow); ?> mb-3 mb-sm-0">
-                    <div class="card h-100">
-                        <?php if ($featuredItem->hasMedia()) { ?>
-                            <img
-                                class="card-img-top"
-                                src="<?= $featuredItem->getThumb([
-                                    'width' => $featuredItem,
-                                    'height' => $featuredItem,
-                                ]); ?>" alt="<?= $featuredItem['menu_name']; ?>"/>
-                        <?php } ?>
-                        <div class="card-body">
-                            <h4 class="card-title">
-                                <?= $featuredItem['menu_name']; ?>
-                                <small><?= currency_format($featuredItem['menu_price']); ?></small>
-                            </h4>
-                            <p class="card-text"><?= $featuredItem['menu_description']; ?></p>
+            <div class="row">
+                <?php foreach ($featuredMenuItems as $featuredItem) { ?>
+                    <div class="col-sm-<?= round(12 / $featuredPerRow); ?> mb-3 mb-sm-0">
+                        <div class="card h-100">
+                            <?php if ($featuredItem->hasMedia()) { ?>
+                                <img
+                                    class="card-img-top"
+                                    src="<?= $featuredItem->getThumb([
+                                        'width' => $featuredItem,
+                                        'height' => $featuredItem,
+                                    ]); ?>" alt="<?= $featuredItem['menu_name']; ?>"/>
+                            <?php } ?>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <?= $featuredItem['menu_name']; ?>
+                                    <small><?= currency_format($featuredItem['menu_price']); ?></small>
+                                </h4>
+                                <p class="card-text"><?= $featuredItem['menu_description']; ?></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php } ?>
+                <?php } ?>
+            </div>
         </div>
     </div>
-</div>
+<?php } ?>
