25559a213757fbc84a7a1d24e8d76e000582d368
SamPoyigi <sam@sampoyigi.com>
2019-12-04 18:14:36 +0000

    Merge subscribe-form into newsletter component default partial

--- components/newsletter/default.php
+++ components/newsletter/default.php
@@ -1,9 +1,18 @@
-<div class="container">
-    <div class="row py-4">
-        <div class="col-lg-6 col-md-8 col-sm-12 m-auto text-center">
-            <div id="newsletter-box">
-                <?= partial('@subscribe-form'); ?>
-            </div>
-        </div>
+<form
+    id="subscribeForm"
+    class="subscribe-form"
+    method="POST" data-request="<?= $subscribeHandler ?>">
+    <div class="input-group subscribe-group">
+        <input
+            type="text"
+            class="form-control"
+            name="subscribe_email"
+        >
+        <span class="input-group-append">
+            <button
+                id="subscribeButton"
+                class="btn btn-info"
+            ><i class="fa fa-paper-plane-o"></i></button>
+        </span>
     </div>
-</div>
+</form>
