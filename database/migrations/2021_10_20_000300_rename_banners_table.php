170290c68c1f0045cb87a53a28b1e5ade9a69fdf
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-03-01 22:52:42 +0000

    Fix migration drop foreign key issue
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- database/migrations/2021_10_20_000300_rename_banners_table.php
+++ database/migrations/2021_10_20_000300_rename_banners_table.php
@@ -3,20 +3,19 @@
 namespace Igniter\FrontEnd\Database\Migrations;
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class RenameBannersTable extends Migration
 {
     public function up()
     {
+        if (Schema::hasTable('igniter_frontend_banners'))
+            return;
+
         Schema::rename('banners', 'igniter_frontend_banners');
     }
 
     public function down()
     {
-        Schema::table('igniter_frontend_banners', function (Blueprint $table) {
-            $table->dropForeign(['language_id']);
-        });
     }
 }
