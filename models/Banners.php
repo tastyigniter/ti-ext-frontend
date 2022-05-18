3e436b6c1e7da4256da1a010bb607f6535432a92
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-05-18 23:58:43 +0100

    Use psr-4 directory structure
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- models/Banners.php
+++ models/Banners.php
@@ -1,106 +1,171 @@
 <?php
-
-namespace Igniter\Frontend\Models;
-
-use Igniter\Flame\Database\Model;
-use Main\Models\Image_tool_model;
-
-/**
- * Banners Model Class
- */
-class Banners extends Model
-{
-    /**
-     * @var string The database table name
-     */
-    protected $table = 'igniter_frontend_banners';
-
-    /**
-     * @var string The database table primary key
-     */
-    protected $primaryKey = 'banner_id';
-
-    protected $fillable = ['name', 'type', 'click_url', 'language_id', 'alt_text', 'image_code', 'custom_code', 'status'];
-
-    public $relation = [
-        'belongsTo' => [
-            'language' => \System\Models\Languages_model::class,
+$config['list']['filter'] = [
+    'search' => [
+        'prompt' => 'lang:igniter.frontend::default.banners.text_filter_search',
+        'mode' => 'all', // or any, exact
+    ],
+    'scopes' => [
+        'status' => [
+            'label' => 'lang:admin::lang.text_filter_status',
+            'type' => 'switch',
+            'conditions' => 'status = :filtered',
         ],
-    ];
+    ],
+];
 
-    protected $casts = [
-        'image_code' => 'serialize',
-        'language_id' => 'integer',
-        'status' => 'boolean',
-    ];
+$config['list']['toolbar'] = [
+    'buttons' => [
+        'create' => [
+            'label' => 'lang:admin::lang.button_new',
+            'class' => 'btn btn-primary',
+            'href' => 'igniter/frontend/banners/create',
+        ],
+        'sliders' => [
+            'label' => lang('igniter.frontend::default.slider.text_title'),
+            'class' => 'btn btn-default',
+            'href' => 'igniter/frontend/sliders',
+            'permission' => 'Igniter.FrontEnd.ManageSlideshow',
+        ],
+    ],
+];
 
-    //
-    // Accessors & Mutators
-    //
+$config['list']['bulkActions'] = [
+    'status' => [
+        'label' => 'lang:admin::lang.list.actions.label_status',
+        'type' => 'dropdown',
+        'class' => 'btn btn-light',
+        'statusColumn' => 'status',
+        'menuItems' => [
+            'enable' => [
+                'label' => 'lang:admin::lang.list.actions.label_enable',
+                'type' => 'button',
+                'class' => 'dropdown-item',
+            ],
+            'disable' => [
+                'label' => 'lang:admin::lang.list.actions.label_disable',
+                'type' => 'button',
+                'class' => 'dropdown-item text-danger',
+            ],
+        ],
+    ],
+    'delete' => [
+        'label' => 'lang:admin::lang.button_delete',
+        'class' => 'btn btn-light text-danger',
+        'data-request-confirm' => 'lang:admin::lang.alert_warning_confirm',
+    ],
+];
 
-    public function getTypeLabelAttribute()
-    {
-        return ucwords($this->type);
-    }
+$config['list']['columns'] = [
+    'edit' => [
+        'type' => 'button',
+        'iconCssClass' => 'fa fa-pencil',
+        'attributes' => [
+            'class' => 'btn btn-edit',
+            'href' => 'igniter/frontend/banners/edit/{banner_id}',
+        ],
+    ],
+    'name' => [
+        'label' => 'lang:admin::lang.label_name',
+        'type' => 'text',
+        'searchable' => true,
+    ],
+    'type_label' => [
+        'label' => 'lang:admin::lang.label_type',
+        'type' => 'text',
+    ],
+    'status' => [
+        'label' => 'lang:igniter.frontend::default.banners.column_status',
+        'type' => 'switch',
+        'searchable' => true,
+    ],
+    'banner_id' => [
+        'label' => 'lang:admin::lang.column_id',
+        'invisible' => true,
+    ],
 
-    //
-    // Scopes
-    //
+];
 
-    public function scopeIsEnabled($query)
-    {
-        return $query->where('status', 1);
-    }
+$config['form']['toolbar'] = [
+    'buttons' => [
+        'back' => [
+            'label' => 'lang:admin::lang.button_icon_back',
+            'class' => 'btn btn-outline-secondary',
+            'href' => 'igniter/frontend/banners',
+        ],
+        'save' => [
+            'label' => 'lang:admin::lang.button_save',
+            'context' => ['create', 'edit'],
+            'partial' => 'form/toolbar_save_button',
+            'class' => 'btn btn-primary',
+            'data-request' => 'onSave',
+            'data-progress-indicator' => 'admin::lang.text_saving',
+        ],
+        'delete' => [
+            'label' => 'lang:admin::lang.button_icon_delete',
+            'context' => ['edit'],
+            'class' => 'btn btn-danger',
+            'data-request' => 'onDelete',
+            'data-request-submit' => 'true',
+            'data-request-confirm' => 'lang:admin::lang.alert_warning_confirm',
+        ],
+    ],
+];
 
-    //
-    // Helpers
-    //
+$config['form']['fields'] = [
+    'name' => [
+        'label' => 'lang:admin::lang.label_name',
+        'type' => 'text',
+    ],
+    'type' => [
+        'label' => 'lang:igniter.frontend::default.banners.label_type',
+        'type' => 'radiotoggle',
+        'default' => 'image',
+        'options' => [
+            'image' => 'lang:igniter.frontend::default.banners.text_image',
+            'custom' => 'lang:igniter.frontend::default.banners.text_custom',
+        ],
+    ],
+    'image_code' => [
+        'label' => 'lang:igniter.frontend::default.banners.label_image',
+        'type' => 'mediafinder',
+        'mode' => 'grid',
+        'commentAbove' => 'lang:igniter.frontend::default.banners.help_image',
+        'isMulti' => true,
+        'trigger' => [
+            'action' => 'hide',
+            'field' => 'type',
+            'condition' => 'value[custom]',
+        ],
+    ],
+    'custom_code' => [
+        'label' => 'lang:igniter.frontend::default.banners.label_custom_code',
+        'type' => 'textarea',
+        'trigger' => [
+            'action' => 'show',
+            'field' => 'type',
+            'condition' => 'value[custom]',
+        ],
+    ],
+    'alt_text' => [
+        'label' => 'lang:igniter.frontend::default.banners.label_alt_text',
+        'type' => 'text',
+    ],
+    'click_url' => [
+        'label' => 'lang:igniter.frontend::default.banners.label_click_url',
+        'type' => 'text',
+        'comment' => 'lang:igniter.frontend::default.banners.help_click_url',
+    ],
+    'language_id' => [
+        'label' => 'lang:igniter.frontend::default.banners.label_language',
+        'type' => 'relation',
+        'relationFrom' => 'language',
+        'placeholder' => 'lang:admin::lang.text_please_select',
+    ],
+    'status' => [
+        'label' => 'lang:admin::lang.label_status',
+        'type' => 'switch',
+        'default' => true,
+    ],
+];
 
-    public function getLanguageIdOptions()
-    {
-        return $this->dropdown('name');
-    }
-
-    public function getImageThumb($options = [])
-    {
-        $defaults = ['name' => 'no_photo.png', 'path' => 'data/no_photo.png', 'url' => $options['no_photo']];
-
-        if (empty($this->image_code))
-            return $defaults;
-
-        $image = unserialize($this->image_code);
-
-        if (empty($image['path']))
-            return $defaults;
-
-        return $this->getThumbArray($image['path'], 120, 120);
-    }
-
-    public function getCarouselThumbs($options = [])
-    {
-        $defaults = [];
-
-        if (empty($this->image_code))
-            return $defaults;
-
-        $image = unserialize($this->image_code);
-
-        if (!is_array($image['paths']))
-            return $defaults;
-
-        foreach ($image['paths'] as $path) {
-            $images[] = $this->getThumbArray($path, 120, 120);
-        }
-
-        return $images;
-    }
-
-    public function getThumbArray($file_path, $width = 120, $height = 120)
-    {
-        return [
-            'name' => basename($file_path),
-            'path' => $file_path,
-            'url' => Image_tool_model::resize($file_path, $width, $height),
-        ];
-    }
-}
+return $config;
---
+++ src/Models/Menu.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Igniter\Frontend\Models;
+
+class Menu extends \Igniter\Cart\Models\Menu
+{
+    public static function getByIds($options = [])
+    {
+        extract(array_merge([
+            'pageLimit' => 20,
+            'sort' => 'menu_priority asc',
+            'menuIds' => [],
+        ], $options));
+
+        if (!is_array($menuIds)) {
+            $menuIds = [$menuIds];
+        }
+
+        $query = self::with('locations')->whereIn('menu_id', $menuIds);
+
+        if (!is_array($sort)) {
+            $sort = [$sort];
+        }
+
+        foreach ($sort as $_sort) {
+            $parts = explode(' ', $_sort);
+            if (count($parts) < 2) {
+                array_push($parts, 'desc');
+            }
+            [$sortField, $sortDirection] = $parts;
+            $query->orderBy($sortField, $sortDirection);
+        }
+
+        return $query->take($pageLimit)->get();
+    }
+}
