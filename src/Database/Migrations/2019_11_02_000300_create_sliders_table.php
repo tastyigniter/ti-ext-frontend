76c1ac0cc4266d5ccc72fc3b459d7453b35aec34
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-05-26 09:42:13 +0100

    wip
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Database/Migrations/2019_11_02_000300_create_sliders_table.php
+++ src/Database/Migrations/2019_11_02_000300_create_sliders_table.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
+
     public function up()
     {
         Schema::create('igniter_frontend_sliders', function (Blueprint $table) {
