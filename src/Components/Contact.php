0f39136a6d83bdd674236715baa96a0cc806721b
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2023-07-30 00:49:46 +0100

    Avoid catching exceptions within Ajax handlers, use exception handlers instead
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Components/Contact.php
+++ src/Components/Contact.php
@@ -2,7 +2,6 @@
 
 namespace Igniter\Frontend\Components;
 
-use Exception;
 use Igniter\Admin\Traits\ValidatesForm;
 use Igniter\Local\Facades\Location;
 use Igniter\Main\Template\Page;
@@ -51,40 +50,34 @@ class Contact extends BaseComponent
 
     public function onSubmit()
     {
-        try {
-            $rules = [
-                ['subject', 'lang:igniter.frontend::default.contact.text_select_subject', 'required|max:128'],
-                ['email', 'lang:igniter.frontend::default.contact.label_email', 'required|email:filter|max:96'],
-                ['full_name', 'lang:igniter.frontend::default.contact.label_full_name', 'required|min:2|max:255'],
-                ['telephone', 'lang:igniter.frontend::default.contact.label_telephone', 'required'],
-                ['comment', 'lang:igniter.frontend::default.contact.label_comment', 'max:1500'],
-            ];
+        $rules = [
+            ['subject', 'lang:igniter.frontend::default.contact.text_select_subject', 'required|max:128'],
+            ['email', 'lang:igniter.frontend::default.contact.label_email', 'required|email:filter|max:96'],
+            ['full_name', 'lang:igniter.frontend::default.contact.label_full_name', 'required|min:2|max:255'],
+            ['telephone', 'lang:igniter.frontend::default.contact.label_telephone', 'required'],
+            ['comment', 'lang:igniter.frontend::default.contact.label_comment', 'max:1500'],
+        ];
 
-            $this->validate(post(), $rules);
+        $this->validate(post(), $rules);
 
-            $data = [
-                'full_name' => post('full_name'),
-                'contact_topic' => post('subject'),
-                'contact_email' => post('email'),
-                'contact_telephone' => post('telephone'),
-                'contact_message' => post('comment'),
-            ];
+        $data = [
+            'full_name' => post('full_name'),
+            'contact_topic' => post('subject'),
+            'contact_email' => post('email'),
+            'contact_telephone' => post('telephone'),
+            'contact_message' => post('comment'),
+        ];
 
-            Mail::queueTemplate('igniter.frontend::mail.contact', $data, [
-                setting('site_email'), setting('site_name'),
-            ]);
+        Mail::queueTemplate('igniter.frontend::mail.contact', $data, [
+            setting('site_email'), setting('site_name'),
+        ]);
 
-            flash()->success(lang('igniter.frontend::default.contact.alert_contact_sent'));
+        flash()->success(lang('igniter.frontend::default.contact.alert_contact_sent'));
 
-            $redirectUrl = $this->controller->pageUrl($this->property('redirectPage'));
+        $redirectUrl = $this->controller->pageUrl($this->property('redirectPage'));
 
-            if ($redirectUrl = get('redirect', $redirectUrl)) {
-                return Redirect::to($redirectUrl);
-            }
-        } catch (Exception $ex) {
-            flash()->warning($ex->getMessage());
-
-            return Redirect::back()->withInput();
+        if ($redirectUrl = get('redirect', $redirectUrl)) {
+            return Redirect::to($redirectUrl);
         }
     }
 }
