e2590daa89fdf4e89275ee20153108149c23ef44
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2023-10-22 14:03:10 +0100

    Clickable featured menu items

--- components/featureditems/default.blade.php
+++ components/featureditems/default.blade.php
@@ -6,24 +6,30 @@
             <div class="row">
                 @foreach ($featuredMenuItems as $featuredItem)
                     <div class="col-sm-{{ round(12 / $featuredPerRow) }} mb-3 mb-sm-0">
-                        <div class="card h-100">
-                            @if ($featuredItem->hasMedia())
-                                <img
-                                    class="card-img-top"
-                                    src="{{ $featuredItem->getThumb([
+                        <a
+                            class="text-decoration-none text-reset"
+                            href="{{page_url('local/menus', ['location' => $featuredItem->locations->first()->permalink_slug])}}?menuId={{ $featuredItem->getBuyableIdentifier() }}"
+                        >
+                            <div class="card h-100">
+                                @if ($featuredItem->hasMedia())
+                                    <img
+                                        class="card-img-top"
+                                        src="{{ $featuredItem->getThumb([
                                         'width' => $featuredWidth,
                                         'height' => $featuredHeight,
                                     ]) }}" alt="{{ $featuredItem->getBuyableName() }}"
-                                />
-                            @endif
-                            <div class="card-body">
-                                <h4 class="card-title">
-                                    {{ $featuredItem->getBuyableName() }}
-                                    <small>{{ currency_format($featuredItem->getBuyablePrice()) }}</small>
-                                </h4>
-                                <p class="card-text">{{ $featuredItem['menu_description'] }}</p>
+                                    />
+                                @endif
+                                <div class="card-body">
+                                    <h4 class="card-title">
+                                        {{ $featuredItem->getBuyableName() }}
+                                        <small>{{ currency_format($featuredItem->getBuyablePrice()) }}</small>
+                                    </h4>
+                                    <h6>{{$featuredItem->locations->implode('location_name', ',')}}</h6>
+                                    <p class="card-text">{{ $featuredItem['menu_description'] }}</p>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                 @endforeach
             </div>
