tastyigniter/ti-ext-translate

Refactor code to use parameter, return type hints
Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

src/Extension.php
@@ -25,7 +25,7 @@ class Extension extends BaseExtension
         resolve(EventRegistry::class)->bootTranslatableModels();
     }
 
-    public function registerFormWidgets()
+    public function registerFormWidgets(): array
     {
         return [
             \Igniter\Translate\FormWidgets\TRLText::class => ['code' => 'trltext'],

tastyigniter/ti-ext-frontend

src/Extension.php
@@ -22,7 +22,7 @@ class Extension extends \Igniter\System\Classes\BaseExtension
         });
     }
 
-    public function registerNavigation()
+    public function registerNavigation(): array
     {
         return [
             'design' => [
@@ -39,7 +39,7 @@ class Extension extends \Igniter\System\Classes\BaseExtension
         ];
     }
 
-    public function registerPermissions()
+    public function registerPermissions(): array
     {
         return [
             'Igniter.FrontEnd.ManageSettings' => [
@@ -57,7 +57,7 @@ class Extension extends \Igniter\System\Classes\BaseExtension
         ];
     }
 
-    public function registerSettings()
+    public function registerSettings(): array
     {
         return [
             'captchasettings' => [
@@ -77,7 +77,7 @@ class Extension extends \Igniter\System\Classes\BaseExtension
         ];
     }
 
-    public function registerMailTemplates()
+    public function registerMailTemplates(): array
     {
         return [
             'igniter.frontend::mail.contact' => 'Contact form email to admin',