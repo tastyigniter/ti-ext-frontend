60af056e663f218d118d8c20815bbc4789cc053c
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-03-29 21:48:45 +0000

    chore(migration): remove DB facade usage for dropping foreign keys and indexes
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- database/migrations/2022_06_30_010000_drop_foreign_key_constraints.php
+++ database/migrations/2022_06_30_010000_drop_foreign_key_constraints.php
@@ -6,7 +6,6 @@ namespace Igniter\Frontend\Database\Migrations;
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -17,7 +16,7 @@ return new class extends Migration
 
         Schema::table('igniter_frontend_banners', function(Blueprint $table): void {
             $table->dropForeignKeyIfExists('language_id');
-            $table->dropIndexIfExists(DB::getTablePrefix().'igniter_frontend_banners_language_id_foreign');
+            $table->dropIndexIfExists('igniter_frontend_banners_language_id_foreign');
         });
 
         Schema::enableForeignKeyConstraints();
