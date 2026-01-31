2fb04ba4a2154ea78061a3ab43bea0d1a13d45b5
sampoyigi <6567634+sampoyigi@users.noreply.github.com>
2026-01-31 18:39:00 +0000

    test: remove unused `User` import from `SlidersTest`

--- tests/Http/Controllers/SlidersTest.php
+++ tests/Http/Controllers/SlidersTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Igniter\Frontend\Tests\Http\Controllers;
 
 use Igniter\Frontend\Models\Slider;
-use Igniter\User\Models\User;
 
 it('loads sliders page', function(): void {
     actingAsSuperUser()
